9f8e5a69c1eed1a23279b8677269b642ef676126
Khushboo186 <197087645+Khushboo186@users.noreply.github.com>
2025-02-04 00:12:46 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -6,10 +6,10 @@ int main() {
     int num1,num2;
     scanf("%d %d",&num1,&num2);
     if(num1>num2){
-        printf("%d is greater than %d\n",num1,num2);
+        printf("%d is greater than %d\n", num1,num2);
     }
     else{
-        printf("%d is not greater than %d\n",num1,num2);
+        printf("%d is not greater than %d\n", num1,num2);
     }
     
     return 0;
