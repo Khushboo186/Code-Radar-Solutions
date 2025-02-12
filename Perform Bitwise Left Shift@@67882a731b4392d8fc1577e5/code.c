2c66a158ec4a8e1464f84db609e678ec1e599550
KHUSHBOO RAIZADA <197087645+Khushboo186@users.noreply.github.com>
2025-02-12 19:01:24 +0530

    Solution to Perform Bitwise Left Shift

---
+++ Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+int num1;
+num = 10  # For example, let's use the integer 10
+shifted_num = num << 2 
+
+# Print the result
+print("Original number:", num)
+print("After left shift operation:", shifted_num)
+}
