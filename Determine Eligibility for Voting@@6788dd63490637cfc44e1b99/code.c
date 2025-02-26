2287e56525f5a8d9f7fdd2f1a784ae44feed45ca
KHUSHBOO RAIZADA <197087645+Khushboo186@users.noreply.github.com>
2025-02-26 13:15:56 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,10 @@
+# include <stdio.h> 
+int main () {
+    int num1;
+    scanf("%d", &num1);
+    if (num1 >= 18){
+        printf("Eligible");
+    } else {
+        printf("Not Eligible");
+    }
+}
