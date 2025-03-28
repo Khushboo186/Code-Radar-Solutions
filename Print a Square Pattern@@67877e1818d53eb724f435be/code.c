ea4da8bc0e9b9be9e7c90267e277e1c9ffdcc1b4
KHUSHBOO RAIZADA <197087645+Khushboo186@users.noreply.github.com>
2025-03-28 20:17:37 +0530

    Solution to Print a Square Pattern

--- Print a Square Pattern@@67877e1818d53eb724f435be/code.c
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -1,13 +1,39 @@
+// #include <stdio.h>
+
+// int main() {
+//     int N, i, j;
+//     scanf("%d", &N);
+//     for (i = 0; i < N; i++) {
+//         for (j = 0; j < N; j++) {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <stdio.h>
 
 int main() {
     int N, i, j;
     scanf("%d", &N);
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            printf("* ");
-        }
-        printf("\n");
+    for(i = 0; i < N; i++){
+        for (j = 0; j < N; j++){
+        printf("* ");
+    } 
+    printf("\n");
     }
     return 0;
 }
