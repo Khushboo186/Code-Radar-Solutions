9d7beb731f2a530be50e18b24dcad78b7f73b8e7
KHUSHBOO RAIZADA <197087645+Khushboo186@users.noreply.github.com>
2025-04-08 22:39:06 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main() {
+    double num1, num2, result;
+    char op;
+    printf("Enter an expression:");
+    scanf("%lf %c %lf", &num1, &op, &num2);
+    switch (op) {
+        case '+':
+        result = num1 + num2;
+        printf("result: %.2lf\n", result);
+        break;
+        case '-':
+        result = num1 - num2;
+        printf("result: %.2lf\n", result);
+        break;
+        case'*';
+        result = num1 * num2;
+        printf("result: %.2lf\n", result);
+        break;
+        case'\';
+        result = num1/num2;
+        printf("result: %.2lf\n", result);
+        break;
+    } else {
+        printf("Error: Division by zero\n");
+    }
+    break;
+    default:
+    printf("Error: Invalid operator\n");
+
+    return 0;
+}
